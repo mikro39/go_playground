176b85ce6f634859611ce296edd84c338cc1f82d
Mike Romero <mikro39@gmail.com>
2023-07-15 00:39:50 -0500

    adding to the loop playground

--- src/loops/loop.go
+++ src/loops/loop.go
@@ -8,4 +8,18 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+
+	fmt.Println("Now for a While loop") //The following sets up a while loop
+	a := 0
+	for a < 5 {
+		fmt.Println(a)
+		a++
+	}
+
+	fmt.Println("Now for looping an array")
+	testarray := []string{"ab", "bb", "cb"}
+
+	for index, value := range testarray {
+		fmt.Println("index", index, "values", value)
+	}
 }
