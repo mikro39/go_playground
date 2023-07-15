7966186c02f8fff2a6882319dc50683d274e92f7
Mike Romero <mikro39@gmail.com>
2023-07-15 00:47:49 -0500

    adding multiple function playground

---
+++ src/multi_function/multifunc.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("color")
+
+	result := sum(2, 5) //values to pass to "sum"
+	fmt.Println(result)
+}
+
+// takes x int and y int and passes them into the results, in this case, the func "sum" returns the operation x + y
+func sum(x int, y int) int {
+	return x + y
+}
