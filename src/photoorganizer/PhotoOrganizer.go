91f6687fd29e3d4946c8cdf1b0379fd626fd1e32
Mike Romero <mikro39@gmail.com>
2023-09-11 22:31:42 -0600

    added file types, readme, docs, and instructions

--- src/photoorganizer/PhotoOrganizer.go
+++ src/photoorganizer/PhotoOrganizer.go
@@ -18,20 +18,30 @@ var photoExtensions = map[string]bool{
 	".jpg":  true,
 	".jpeg": true,
 	".png":  true,
-	".cr3":  true,
 	".cr2":  true,
+	".cr3":  true,
+	".tif":  true,
+	".tiff": true,
+	".nef":  true,
+	".orf":  true,
+	".dng":  true,
+	".arw":  true,
 }
 
 var videoExtensions = map[string]bool{
-	".mp4": true,
-	".avi": true,
-	".mov": true,
-	".mkv": true,
+	".mp4":  true,
+	".avi":  true,
+	".mov":  true,
+	".mkv":  true,
+	".flv":  true,
+	".wmv":  true,
+	".m4v":  true,
 }
 
 var wg sync.WaitGroup
 var mu sync.Mutex
-var counter int
+var counter, totalFiles int
+var semaphore = make(chan struct{}, 2) // limit to 2 goroutines at once
 
 func isPhotoOrVideo(filename string) bool {
 	ext := strings.ToLower(filepath.Ext(filename))
@@ -77,8 +87,11 @@ func getDateTaken(path string) (int, int, error) {
 	return 0, 0, fmt.Errorf("Unsupported file extension: %s", ext)
 }
 
-func processFile(path string, info os.FileInfo) {
+func processFile(path string) {
 	defer wg.Done()
+	defer func() {
+		<-semaphore // release a slot
+	}()
 
 	if isPhotoOrVideo(path) {
 		year, month, err := getDateTaken(path)
@@ -88,37 +101,76 @@ func processFile(path string, info os.FileInfo) {
 		}
 
 		dstDir := filepath.Join("./media", fmt.Sprintf("%d", year), fmt.Sprintf("%02d", month))
+		if filepath.Dir(path) == dstDir {
+			fmt.Printf("%s is already in the correct location\n", path)
+			return
+		}
+
 		if err := os.MkdirAll(dstDir, os.ModePerm); err != nil {
 			fmt.Printf("Failed to create directory %s: %s\n", dstDir, err)
 			return
 		}
 
-		dstPath := filepath.Join(dstDir, info.Name())
+		dstPath := filepath.Join(dstDir, filepath.Base(path))
 		if err := os.Rename(path, dstPath); err != nil {
 			fmt.Printf("Failed to move %s to %s: %s\n", path, dstPath, err)
 			return
 		} else {
 			mu.Lock()
 			counter++
+			printProgress(counter, totalFiles)
 			mu.Unlock()
-			fmt.Printf("Moved %s to %s\n", path, dstPath)
 		}
 	}
 }
 
+func printProgress(count, total int) {
+	const barLength = 40
+	progress := float64(count) / float64(total)
+	bars := int(progress * barLength)
+	fmt.Printf("\r[%s%s] %d/%d", strings.Repeat("=", bars), strings.Repeat(" ", barLength-bars), count, total)
+}
+
 func main() {
-	srcDir := "./media"
+	// Welcome message
+	fmt.Println("=============================================")
+	fmt.Println("   Mike's Photo & Video Sorting Tool!")
+	fmt.Println("=============================================")
+	fmt.Println("This tool organizes your photos and videos by extracting the 'date taken' metadata and sorting them into folders by year and month.")
+	fmt.Println("For accurate results, please ensure that 'exiftool' is installed on your system.")
+	fmt.Println("\nCo-coded with ChatGPT from OpenAI! 🚀")
+	fmt.Println("\nLet's get started!")
+	fmt.Println("---------------------------------------------\n")
+	
+	fmt.Println("Which folder would you like to sort?")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	srcDir := scanner.Text()
 
 	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
-		if !info.IsDir() {
-			wg.Add(1)
-			go processFile(path, info)
+		if !info.IsDir() && isPhotoOrVideo(path) {
+			totalFiles++
 		}
+		return nil
+	})
 
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	err = filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			semaphore <- struct{}{} // acquire a slot
+			wg.Add(1)
+			go processFile(path)
+		}
 		return nil
 	})
 
@@ -128,7 +180,7 @@ func main() {
 		fmt.Println("Error:", err)
 	}
 
-	fmt.Printf("\nMoved %d files.\n", counter)
+	fmt.Printf("\nMoved %d files out of %d.\n", counter, totalFiles)
 	fmt.Println("Press any key to continue...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
